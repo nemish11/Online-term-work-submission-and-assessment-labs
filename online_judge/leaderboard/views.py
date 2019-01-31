e6e7221398c9c19cc159de7d46517f03ac7e8db7
ckrokad <chintanrokad932@gmail.com>
2019-01-31 23:34:12 +0530

    leader board view added

--- online_judge/leaderboard/views.py
+++ online_judge/leaderboard/views.py
@@ -14,19 +14,32 @@ from userprofile.models import Faculty, Student
 def show_leaderboard(request):
     subject = Subject.objects.all().first()
     weeks = Week.objects.filter(subject=subject)
-    students = Request.objects.filter(subject=subject,status="approved")
+    students = Request.objects.filter(subject=subject, status="approved")
+    submision_list = Submission.objects.all()
     dic = {}
     for s in students:
-        dic[s.student] = {}
-    for key in dic:
+        dic[s.student.user] = {}
+        t = {}
+        t['total_score'] = 0
+        for w in weeks:
+            t[w.name] = 0
+        dic[s.student.user] = t
+    '''for key in dic:
         d = dic[key]
         for w in weeks:
 
             assign = Assignment.objects.filter(week=w, subject=subject,Submission__user=request.user).sum('totalscore')
 
             d[w.name] = assign
-            print(key.user.username, w.name, assign)
-    c={}
+            print(key.user.username, w.name, assign)'''
+    for s in submision_list:
+        assign = s.assignment
+        w = assign.week.name
+        if dic[s.user][w] > s.totalscore and assign.subject == subject:
+            dic[s.user]['total_score'] -= dic[s.user][w] + s.totalscore
+            dic[s.user][w] = s.totalscore
+
+    c = {}
     c['data'] = dic
     c['weeks'] = weeks
     c['subject'] = subject
